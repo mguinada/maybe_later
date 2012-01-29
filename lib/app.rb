e36d504db3f682c4333cc1d07d098e39dc8687e8
Miguel Guinada <mguinada@gmail.com>
2012-01-29 18:10:29 +0000

    Added the 1140 grid system and some basic style

--- lib/app.rb
+++ lib/app.rb
@@ -6,6 +6,7 @@ Bundler.require :default, Sinatra::Application.environment
 
 class Application < Sinatra::Base
   set :views, File.dirname(__FILE__)  + "/../views"
+  set :public_folder, File.dirname(__FILE__) + "/../public"
 
   get '/' do
     haml :welcome
